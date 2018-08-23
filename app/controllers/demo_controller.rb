21ef01b131a4c637f4ee72e28a96c77a4d52b388
Rusli <tan.rusli@outlook.com>
2018-08-23 22:11:26 +1000

    test

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,20 @@
 class DemoController < ApplicationController
   def index
   end
+
+  def create 
+  end 
+
+  def new 
+  end 
+
+  def show 
+  end 
+
+  def edit 
+  end 
+
+  def delete
+  end 
+
 end
