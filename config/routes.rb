4c73386b1379066bf5921ea5d37867578faa8905
Rusli <tan.rusli@outlook.com>
2018-09-05 23:51:48 +1000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'demo#index'
 
-  resources :subjects 
+  resources :subjects do 
+  	member do 
+  		get :delete
+  	end 
+  end 
   
 
-  get 'demo/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
