a09cb60e784e57495a14a7add3e397a2bb028786
Rusli <tan.rusli@outlook.com>
2018-11-11 22:01:54 +1100

    cms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'demo#index'
+  root 'subjects#index'
 
   resources :subjects do 
   	member do 
@@ -8,6 +8,4 @@ Rails.application.routes.draw do
   end 
   
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
